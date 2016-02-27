144e169305979a00b84b716980b60df706aab94b
Tim <TG908@users.noreply.github.com>
2016-02-27 13:20:15 +0100

    Update index.js

--- index.js
+++ index.js
@@ -32,7 +32,7 @@ VSX.prototype.getOn = function(callback) {
   client.connect(PORT, HOST, function() {
    
     console.log('CONNECTED TO: ' + HOST + ':' + PORT);
-    client.write('P?\r\n');
+    client.write('?P\r\n');
 
 }); 
     
