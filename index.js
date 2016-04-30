d39e58f54576a565321966f6893b6222bd3a5577
Tim <TG908@users.noreply.github.com>
2016-04-30 12:24:31 +0200

    Update index.js

--- index.js
+++ index.js
@@ -1,5 +1,6 @@
 var Service, Characteristic;
-var raspi = require('raspi-llio');
+var PiFastGpio = require('pi-fast-gpio');
+var gpio = new PiFastGpio();
 
 
 module.exports = function(homebridge) {
@@ -12,8 +13,19 @@ module.exports = function(homebridge) {
 function LED(log, config) {
   this.log = log;
   this.name = config.name;
-  this.HOST = "192.168.178.7";
-  this.PORT = 200;
+  this.h = 0;
+  this.s = 0;
+  this.l = 0;
+  this.LED_1_GPIO = 17;
+  this.LED_2_GPIO = 22;
+  this.LED_3_GPIO = 24;
+  this.HOST = '127.0.0.1';
+  this.PORT = 8888;
+  
+  	gpio.connect(HOST, PORT, function(err) {
+  		if (err) throw err;
+
+	})
 }
 
 LED.prototype.getServices = function() {
@@ -41,29 +53,79 @@ lightbulbService
 }
 
 LED.prototype.setHue = function(hue, callback) {
-	var pwm = new raspi.PWM();
-	raspi.PWM.setMode(0);
-	raspi.PWM.setClockDivisor(400);
-	raspi.PWM.setRange(1000);
-
-	var value = 40;
-	pwm.write(value);
-
+	
+	var r = 0
+	var g = 0
+	var b = 0
+	this.h = hue
+	
+	var rgb = hslToRgb(hue, this.s, this.l)
+	gpio.setPwmDutycycle(LED_1_GPIO, rgb[0]);
+	gpio.setPwmDutycycle(LED_2_GPIO, rgb[1]);
+	gpio.setPwmDutycycle(LED_3_GPIO, rgb[2]);
+	
 
 	 callback();
 }
 
 LED.prototype.setBrightness = function(brightness, callback) {
  
+ 	var r = 0
+	var g = 0
+	var b = 0
+	this.l = brightness
+	
+	var rgb = hslToRgb(this.h, this.s, brightness)
+	
+	gpio.setPwmDutycycle(LED_1_GPIO, rgb[0]);
+	gpio.setPwmDutycycle(LED_2_GPIO, rgb[1]);
+	gpio.setPwmDutycycle(LED_3_GPIO, rgb[2]);
+ 
   callback();
 }
 
 LED.prototype.setSaturation = function(saturation, callback) {
   
+  	var r = 0
+	var g = 0
+	var b = 0
+	this.s = saturation
+	
+	var rgb = hslToRgb(this.hue, saturation, this.l)
+	
+	gpio.setPwmDutycycle(LED_1_GPIO, rgb[0]);
+	gpio.setPwmDutycycle(LED_2_GPIO, rgb[1]);
+	gpio.setPwmDutycycle(LED_3_GPIO, rgb[2]);
+	
   callback();
 }
 
 LED.prototype.setOn = function(on, callback) {
 
   callback();
+}
+
+LED.prototype.hslToRgb = function(h, s, l){
+    var r, g, b;
+
+    if(s == 0){
+        r = g = b = l; // achromatic
+    }else{
+        var hue2rgb = function hue2rgb(p, q, t){
+            if(t < 0) t += 1;
+            if(t > 1) t -= 1;
+            if(t < 1/6) return p + (q - p) * 6 * t;
+            if(t < 1/2) return q;
+            if(t < 2/3) return p + (q - p) * (2/3 - t) * 6;
+            return p;
+        }
+
+        var q = l < 0.5 ? l * (1 + s) : l + s - l * s;
+        var p = 2 * l - q;
+        r = hue2rgb(p, q, h + 1/3);
+        g = hue2rgb(p, q, h);
+        b = hue2rgb(p, q, h - 1/3);
+    }
+
+    return [Math.round(r * 255), Math.round(g * 255), Math.round(b * 255)];
 }
