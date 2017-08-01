4934231340fe0657af3f962c518ac17fe9f83754
Tim <TG908@users.noreply.github.com>
2017-08-01 20:53:07 +0200

    Update index.js

--- index.js
+++ index.js
@@ -64,9 +64,7 @@ LED.prototype.setHue = function(hue, callback) {
 
 LED.prototype.setBrightness = function(brightness, callback) { 
 	
-	this.log(brightness)
-	l = brightness*2;
-	this.log(l)
+	l = brightness/200;
 	var rgb = hslToRgb(h, s, l);
 
 	redLED.pwmWrite(rgb[0]);
